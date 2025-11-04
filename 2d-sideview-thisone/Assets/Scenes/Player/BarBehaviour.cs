d16c8bf9101696e3898a4635d4f9a70f72a0f0e8
leev-sketch <leevi.takalo@elev.ga.lbs.se>
2025-11-04 12:05:12 +0100

    Bars

---
+++ 2d-sideview-thisone/Assets/Scenes/Player/BarBehaviour.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarBehaviour : MonoBehaviour
+{
+    public float Cold, MaxCold, Width, Height;
+
+    [SerializeField]
+    private RectTransform ColdBar;
+
+    public void SetMaxCold(float maxCold)
+    {
+        MaxCold = maxCold;
+    }
+
+    public void SetCold(float cold)
+    {
+        Cold = cold;
+        float newWidth = (Cold / MaxCold) * Width;
+
+        ColdBar.sizeDelta = new Vector2(newWidth, Height);
+    }
+}

7445dcb596c3e55169558bb2da61bdb67d1b4cbf
Agusti087 <oskar@kullgren.org>
2025-11-04 10:35:44 +0100

    Reapply "Reapply "Reapply "idk what this is tbh"""
    This reverts commit 0d2d5f0222a71951cec1a9378512b6481a146bfb.

---
+++ 2d-sideview-thisone/Assets/Scenes/Player/MainUIscript.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class MainUIscript : MonoBehaviour
+{
+    [SerializeField] private RectTransform menu;
+
+    private bool menuVisible = false; // håller koll på om menyn är nere eller uppe
+
+    void Update()
+    {
+        // När man trycker på E
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            menuVisible = !menuVisible; // växla mellan true/false
+
+            Vector2 newPos = menu.anchoredPosition;
+
+            if (menuVisible)
+                newPos.y = 0f; // visa menyn
+            else
+                newPos.y = 321f; // göm menyn offscreen
+
+            menu.anchoredPosition = newPos;
+        }
+    }
+}
